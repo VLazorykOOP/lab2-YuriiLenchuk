a4c031c3d2d65bbe4f6d72e3adf6213ea951c462
YuriiLenchuk <lenchuk.yurii@chnu.edu.ua>
2023-03-27 16:00:49 +0300

    Task1

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <clocale>
 using namespace std;
 #include "Tasks.h"
 #include "Examples.h"
@@ -13,9 +14,18 @@ void MenuTask()
     cout << "    6.  Exit \n";
 }
 void task1() {
+    setlocale(LC_CTYPE, "ukr");
     // Обчислення виразів з використанням побітових операцій
-    // Calculation of expressions using bitwise operations 
-    cout << " Calculation of expressions using bitwise operations  \n";
+    // Calculation of expressions using bitwise operations
+    cout << " Calculation of expressions using bitwise operations  \n" << endl;
+    int a, b, c, d, x, y;
+    cout << "Введіть a, b, c, d: ";
+    cin >> a >> b >> c >> d;
+    x = (b + (b << 5)) + (((d << 4) - d + ((a << 3) + (a << 2))) >> 9) - (c + (c << 6)) + ((d << 4) - (d << 1));
+    y = 33 * b + (d * 15 + 12 * a) / 512 - 65 * c + d * 14;
+    cout << "x= " << x << endl << "y= " << y << endl;
+    cin.get();
+    return;
 }
 
 void task2()
